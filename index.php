e2b9219799d40d2804ebb857f56ab07744500c1e
Daniel Misi <misi@MacBook-Air-de-Daniel.local>
2025-01-12 14:53:22 +0100

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>    
+        <title>GUN</title>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/style/style.css" rel="stylesheet"/>
+        <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        <div class="game">
+            <div class="ennemi"></div>
+        </div>
+    </body>
+</html>
